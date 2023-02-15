6261c76d9718424769fe2a7e4a1195a09f9c0964
gabrielolisilva <gabrielsilva2799@hotmail.com>
2023-02-15 17:07:02 -0300

    Side buttons working

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const btnsControl = document.querySelectorAll(".control");
 
+btnsControl.forEach((singleButton) => {
+  singleButton.addEventListener("click", () => {
+    const activeBtn = document.querySelector(".active-btn");
+    activeBtn.classList.remove("active-btn");
+
+    singleButton.classList.add("active-btn");
+  });
+});
